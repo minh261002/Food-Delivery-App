3ce7f0488e68f5ee0dd714e4271b7cafcf5ead4c
Minh Tran <trcongminh.261002@gmail.com>
2025-09-15 20:40:53 +0700

    Add devtools_options.yaml for DevTools settings and update image URLs in uidata.dart

--- lib/constants/uidata.dart
+++ lib/constants/uidata.dart
@@ -4,7 +4,7 @@ List<dynamic> categories = [
     "title": "Fried Rice",
     "value": "fried_rice",
     "imageUrl":
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/4a4cd06e-94de-4478-8588-66eee01354d4-rice.png",
+        "https://res.cloudinary.com/doy3slx9i/image/upload/v1732117369/Delivery/Bento_Box_yklw8r.png",
     "createdAt": "2023-10-24T16:12:23.571Z",
     "updatedAt": "2023-10-24T16:12:23.571Z",
     "__v": 0,
@@ -14,7 +14,7 @@ List<dynamic> categories = [
     "title": "Curry",
     "value": "curry",
     "imageUrl":
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/ee2d952c-1d7d-48f7-81b3-cb967343eb6c-curry.png",
+        "https://res.cloudinary.com/doy3slx9i/image/upload/v1732117369/Delivery/curry_brepvt.png",
     "__v": 0,
   },
   {
@@ -22,7 +22,7 @@ List<dynamic> categories = [
     "title": "Pizza",
     "value": "pizza",
     "imageUrl":
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/466693e6-f999-42e0-8794-09325ece1b45-Pizza.png",
+        "https://res.cloudinary.com/doy3slx9i/image/upload/v1732117369/Delivery/Pizza_dwzfpr.png",
     "__v": 0,
   },
   {
@@ -30,7 +30,7 @@ List<dynamic> categories = [
     "title": "Pasta",
     "value": "pasta",
     "imageUrl":
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/6645cf41-bc89-4dd0-9f80-bba9487633b2-Spaghetti.png",
+        "https://res.cloudinary.com/doy3slx9i/image/upload/v1732117369/Delivery/Spaghetti_re9qxy.png",
     "__v": 0,
   },
   {
@@ -38,7 +38,7 @@ List<dynamic> categories = [
     "title": "Beverages",
     "value": "beverages",
     "imageUrl":
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/93c4b3df-9e55-4308-a834-9fe4ad67c0b3-bar.png",
+        "https://res.cloudinary.com/doy3slx9i/image/upload/v1732117368/Delivery/bar_pfsu06.png",
     "__v": 0,
   },
   {
@@ -46,7 +46,7 @@ List<dynamic> categories = [
     "title": "Burgers",
     "value": "burgers",
     "imageUrl":
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/3c94bc09-b8b9-4961-accb-f68b7e8121cc-Hamburger.png",
+        "https://res.cloudinary.com/doy3slx9i/image/upload/v1732117369/Delivery/Hamburger_srvrca.png",
     "__v": 0,
   },
   {
@@ -54,7 +54,7 @@ List<dynamic> categories = [
     "title": "Chicken",
     "value": "chicken",
     "imageUrl":
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/139718b2-2832-40c5-8d7b-11fd9feb0aac-chicken.png",
+        "https://res.cloudinary.com/doy3slx9i/image/upload/v1732117369/Delivery/chicken_zogmwe.png",
     "__v": 0,
   },
   {
@@ -62,7 +62,7 @@ List<dynamic> categories = [
     "title": "More",
     "value": "more",
     "imageUrl":
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/08323562-a3c6-461d-95a3-b3817c99871b-more.png",
+        "https://res.cloudinary.com/doy3slx9i/image/upload/v1732117369/Delivery/more_lcvek8.png",
     "__v": 0,
   },
 ];
